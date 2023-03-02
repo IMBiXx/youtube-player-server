6516a79d8aad4e22ff08eac4bb6cbd5251cbb67b
Edweird <wowbixx@gmail.com>
2023-03-02 13:18:09 +0100

    Clean base datas

--- src/data.ts
+++ src/data.ts
@@ -1,14 +1,4 @@
 import { Video } from '~~/types/videos';
 
-export const history: Video[] = [
-  {
-    name: 'Base history 1',
-    url: 'youtube.com',
-  },
-];
-export const bookmark: Video[] = [
-  {
-    name: 'Base bookmark 1',
-    url: 'youtube.com',
-  },
-];
+export const history: Video[] = [];
+export const bookmark: Video[] = [];
